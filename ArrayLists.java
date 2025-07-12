f656d73a5757da5302fc0f38f5b2583f40412e9f
Shashank R B <rbshashank1@gmail.com>
2025-07-12 09:54:52 +0530

    Wrappeer classes

---
+++ ArrayLists.java
@@ -0,0 +1,13 @@
+public class ArrayLists {
+    public static void main(String[] args) {
+        
+        // Wrapper classes 
+        Integer i = Integer.valueOf(4);
+        System.out.println(i);
+
+        Float f = Float.valueOf(3.00f);
+        System.out.println(f);
+
+        
+    }
+}

e10442fe3d112ed30d89d931a25f26880c7d1872
Shashank R B <rbshashank1@gmail.com>
2025-07-14 21:09:19 +0530

    Printed 1 to n num using recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void printIncreasing(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        printIncreasing(n - 1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {  // corrected method name
+        System.out.print("Enter the num to get 1 to n :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printIncreasing(n);  // added function call
+    }
+}
